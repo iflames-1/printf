d3aa306b6f56d58bbeb2aee7d79e0e533bda62ea
Isaac <isaacomenuche@gmail.com>
2023-08-20 00:56:43 +0100

    my_printf_test

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main(void)
+{
+    int len;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    _printf("Length: [%d]\n", len);
+    _printf("Character: [%c]\n", 'H');
+    _printf("String: [%s]\n", "I am a string !");
+    _printf("Percent: [%%]\n");
+    return (0);
+}
