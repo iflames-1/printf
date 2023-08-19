5431bcb67914415988e039f822e3e07d5374482e
Isaac <isaacomenuche@gmail.com>
2023-08-19 20:33:10 +0100

    Function that produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	int printed_chars = 0;
+	/* code to parse format and handle conversions goes here */
+	return (printed_chars);
+}
---
+++ test-flies/README.md
@@ -0,0 +1,1 @@
+This dir contains all the tes file
